190112bd77e4b0fd2cb0f39dcfa1ec5a349a710b
Vinh Le <ldvinh596@gmail.com>
2025-08-31 21:29:53 +0700

    feature: implement companies module with CRUD operations and DTOs

---
+++ src/companies/dto/create-company.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateCompanyDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Address is required' })
+  address: string;
+
+  @IsNotEmpty({ message: 'Description is required' })
+  description: string;
+}
