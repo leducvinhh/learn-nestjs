2f78eb7901496ac70a118e875c4ad4a9e826346c
Vinh Le <ldvinh596@gmail.com>
2025-09-21 11:22:27 +0700

    feat: enhance CreateJobDto and Jobs module with company details and user context in job operations

--- src/jobs/dto/create-job.dto.ts
+++ src/jobs/dto/create-job.dto.ts
@@ -1,25 +1,33 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsArray,
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
+
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: mongoose.Schema.Types.ObjectId;
+}
 
 export class CreateJobDto {
   @IsString()
   @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
-  @IsString({ each: true })
+  @IsArray({ message: 'Skills must be an array of strings' })
+  @IsString({ each: true, message: 'Each skill must be a string' })
   @IsNotEmpty({ each: true, message: 'Each skill must be a non-empty string' })
   skills: string[];
-
-  @IsString()
-  @IsNotEmpty({ message: 'Company ID is required' })
-  companyId: string;
-
-  @IsString()
-  @IsNotEmpty({ message: 'Company name is required' })
-  companyName: string;
-
-  @IsString()
-  @IsNotEmpty({ message: 'Location is required' })
-  location: string;
 
   @IsNotEmpty({ message: 'Salary is required' })
   salary: number;
@@ -35,11 +43,23 @@ export class CreateJobDto {
   @IsNotEmpty({ message: 'Description is required' })
   description: string;
 
-  @IsString()
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+
   @IsNotEmpty({ message: 'Start date is required' })
+  @Transform(({ value }) => new Date(value))
+  @IsDate({ message: 'startDate must be a valid date string' })
   startDate: Date;
 
-  @IsString()
-  @IsNotEmpty({ message: 'End date is required' })
+  @IsNotEmpty({ message: 'Start date is required' })
+  @Transform(({ value }) => new Date(value))
+  @IsDate({ message: 'endDate must be a valid date string' })
   endDate: Date;
+
+  @IsBoolean()
+  @IsNotEmpty({ message: 'isActive is required' })
+  isActive: boolean;
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,26 +1,92 @@
 import { Injectable } from '@nestjs/common';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Job, JobDocument } from './schemas/job.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/user.interface';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class JobsService {
-  create(createJobDto: CreateJobDto) {
-    return 'This action adds a new job';
+  constructor(
+    @InjectModel(Job.name)
+    private jobModel: SoftDeleteModel<JobDocument>,
+  ) {}
+
+  create(createJobDto: CreateJobDto, user: IUser) {
+    const newData = {
+      ...createJobDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    };
+
+    return this.jobModel.create(newData);
   }
 
-  findAll() {
-    return `This action returns all jobs`;
+  async findAll(currentPage: string, limit: string, qs: string) {
+    const { filter, population, sort } = aqp(qs);
+
+    delete filter.current;
+    delete filter.pageSize;
+
+    let defaultLimit = +limit ? +limit : 10;
+    let offset = (+currentPage - 1) * +defaultLimit;
+
+    const totalItems = (await this.jobModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.jobModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage,
+        pageSize: limit,
+        pages: totalPages,
+        total: totalItems,
+      },
+      result,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} job`;
+  findOne(id: string) {
+    return this.jobModel.findOne({ _id: id });
   }
 
-  update(id: number, updateJobDto: UpdateJobDto) {
-    return `This action updates a #${id} job`;
+  async update(id: string, updateJobDto: UpdateJobDto, user: IUser) {
+    const payload = {
+      ...updateJobDto,
+      updatedBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    };
+
+    const result = await this.jobModel.updateOne({ _id: id }, payload);
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} job`;
+  async remove(id: string, user: IUser) {
+    await this.jobModel.updateOne(
+      {
+        _id: id,
+      },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+
+    return this.jobModel.softDelete({ _id: id });
   }
 }
